c3d4fe7c5e72da94370d3a79f8b3313e5c62d4aa
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-10-07 11:04:23 -0700

    Implemented basic python file structure

---
+++ TcpAnalysis.py
@@ -0,0 +1,30 @@
+from struct import *
+import sys
+
+def printOutput():
+    print(f'A) Total number of connections:\n --------------------------------\n')
+
+    print(f"B) Connections' details: \n --------------------------------\n")
+
+    print(f'C) General: \n --------------------------------\n')
+
+    print(f'D) Complete TCP connections: \n --------------------------------\n')
+
+def main():
+    #Get file input (tracefile)
+
+    #Decode file input (Use struct.unpack(format type, file input bytes))
+
+    #Process data for part A
+
+    #Process data for part B
+
+    #Process data for part C
+
+    #Process data for part D
+
+    #Print all data
+    printOutput()
+
+if __name__ == "__main__":
+    main()
