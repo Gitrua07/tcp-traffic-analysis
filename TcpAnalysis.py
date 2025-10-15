01bfb1756df8e5f0391a2c97a2252843a4923a4a
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-10-14 19:00:59 -0700

    Implemented getTotalConnections by calculating packets and implement connection details

--- TcpAnalysis.py
+++ TcpAnalysis.py
@@ -6,6 +6,7 @@ PCAP_FILE = './sample-capture-file.cap'
 
 def getCapFile(file):
     #parse using struct
+    ip_packet_list = []
     with open(PCAP_FILE, 'rb') as f:
         global_header = f.read(24)
         if len(global_header) < 24:
@@ -39,18 +40,43 @@ def getCapFile(file):
             packet_count += 1
 
             ip_packet = IPPacket.IPPacket.from_bytes(packet_data[14:])
-
-    print(f'ip_packet = {ip_packet}')
-    print(f'packet_count: {packet_count}, packet_data: {packet_data}, packet_header: {packet_header}')
-
+            ip_packet_list.append((ts_sec, ts_usec, ip_packet))
     
-    return (0,0,0,packet_data)
+    return ip_packet_list
 
 def getTotalConnections(partA):
-    return 0
+    num_connections = len(partA)
+    return num_connections
 
 def getConnectionsDetails(partB):
-    return 0
+    output_list = []
+    count = 1
+    for ip_packet in partB:
+        output = 'Connection ' + str(count) + ': \nSource Address: '
+        src_ip = ip_packet[2].src_ip
+        output = output + src_ip + '\n' + 'Destination Address: '
+        dst_ip = ip_packet[2].dst_ip
+        output = f'{output}{dst_ip}\n' 
+        src_port = ''
+        #output = output + src_port + '\n'
+        dst_port = ''
+        #output = output + dst_port + '\n'
+        status = ''
+        if count == 1:
+            start_time = ip_packet[0]
+            output = f'{output} Start Time: {start_time}'
+        
+        if count == len(partB):
+            end_time = ip_packet[0]
+            output = f'{output} End Time: {end_time}'
+
+        duration = ''
+
+
+        output_list.append(output)
+        count = count + 1
+
+    return output_list
 
 def getGeneralInfo(partC):
     return 0
@@ -64,7 +90,8 @@ def printOutput(A, B, C, D):
     print(A)
     #Part B
     print(f"\nB) Connections' details: \n --------------------------------\n")
-    print(B)
+    for detailB in B:
+        print(detailB)
     #Part C
     print(f'\nC) General: \n --------------------------------\n')
     print(C)
@@ -76,15 +103,15 @@ def main():
     #Get file input (tracefile)
     file = sys.argv[1]
     #Decode file input (Use struct.unpack(format type, file input bytes))
-    partA, partB, partC, partD = getCapFile(file)
+    packet = getCapFile(file)
     #Process data for part A
-    A = getTotalConnections(partA)
+    A = getTotalConnections(packet)
     #Process data for part B
-    B = getConnectionsDetails(partB)
+    B = getConnectionsDetails(packet)
     #Process data for part C  
-    C = getGeneralInfo(partC)
+    C = getGeneralInfo(packet)
     #Process data for part D
-    D = getPartD(partD)
+    D = getPartD(packet)
     #Print all data
     printOutput(A, B, C, D)
 
